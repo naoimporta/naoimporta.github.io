0a5b02b28702c32a9a8556266daa39972e762e60
Fábio Luciano <fabio@naoimporta.com>
2017-06-22 23:44:26 -0300

    Adicionando versão inicial

---
+++ content/post/01-primeiro-blog.md
@@ -0,0 +1,8 @@
++++
+title = "Desistindo oficialmente do wordpress"
+description = "Insuportável"
+date = 2017-06-22T20:00:00Z
++++
+
+
+Com este post, desisto oficialmente do wordpress.
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+REPO="naoimporta.github.io"
+GH_REPO="github.com/naoimporta/${REPO}.git"
+git config --global user.email "fabio@naoimporta.com"
+git config --global user.name "Fabio Luciano"
+
+git clone --recursive -b development 'https://'$GH_REPO development
+git clone -b master 'https://'$GH_REPO master
+hugo --source development --theme=blank
+find ./master -not -path '*/\.git*' -not -path './master' -exec rm -fR {} \;
+mv development/public/* master
+cd master
+git add -A :/
+git commit -a -m "Travis Deploy"
+git push "https://${GH_TOKEN}@${GH_REPO}" master > /dev/null 2>&1
