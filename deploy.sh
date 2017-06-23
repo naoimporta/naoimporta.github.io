5e89cfd4f1568a0481945a49a9d7c43a97c3fb86
Fábio Luciano <fabio@naoimporta.com>
2017-06-22 23:51:03 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ hugo --source development --theme=blank
 find ./master -not -path '*/\.git*' -not -path './master' -exec rm -fR {} \;
 mv development/public/* master
 cd master
+
 git add -A :/
 git commit -a -m "Travis Deploy"
 git push "https://${GH_TOKEN}@${GH_REPO}" master > /dev/null 2>&1
